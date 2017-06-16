af74c55f01cb8a4801bfee0d7fa09ea428f6d5c3
Nico Jansen <jansennico@gmail.com>
2017-06-16 17:15:20 +0200

    301 Moved

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { TestFrameworkFactory } from 'stryker-api/test_framework';
-import JasmineTestFramework from './JasmineTestFramework';
-
-TestFrameworkFactory.instance().register('jasmine', JasmineTestFramework);
